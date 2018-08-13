34238a6b22d7f8c80eed10029fd4af9877366d09
rwieberdink <rebeccawieberdink@gmail.com>
2018-08-13 16:59:00 -0400

    pics

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friendsArray = [
     {
         "name": "Allison",
-        "photo": "/images/woman5.jpg",
+        "photo": "https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?cs=srgb&dl=person-woman-model-38554.jpg&fm=jpg",
         "scores": ["1", "2", "3", "5", "5", "5", "4", "3", "2", "1"]
     },{
         "name": "Becky",
-        "photo": "/images/woman4.jpg",
+        "photo": "http://qige87.com/data/out/92/wp-image-142777024.jpg",
         "scores": ["1", "2", "3", "4", "5", "5", "3", "2", "2", "1"]
     },{
         "name": "Cate",
-        "photo": "/images/woman3.jpg",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7-V23GFsfYB3IB3fOtsVYAKv9gbmC21JXTdD26Hw5L0kmz8Kn",
         "scores": ["4", "5", "4", "1", "1", "1", "2", "3", "5", "3"]
     },{
         "name": "Andrew",
-        "photo": "/images/male3.jpg",
+        "photo": "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350",
         "scores": ["5", "5", "3", "2", "3", "3", "2", "4", "2", "4"]
     },{
         "name": "Robert",
-        "photo": "/images/male1.jpeg",
+        "photo": "https://www.law.virginia.edu/system/files/admissions/ben-cabranes-270.jpg",
         "scores": ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
     },{
         "name": "John",
-        "photo": "/images/male3.jpg",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/a/a1/NafSadh_Profile.jpg",
         "scores": ["4", "2", "4", "2", "4", "4", "4", "2", "4", "4"]
     }
 ];
